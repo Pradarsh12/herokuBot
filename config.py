623cc5bbeb5cbb3f81704a41f63a83b7a3ac2599
modbots <77435462+modbots@users.noreply.github.com>
2021-06-25 22:09:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
+    BOT_TOKEN = "1406408953:AAHOuBQocs7-HsexyYyc_rprbt0C-S368sw"
+    HEROKU_API_KEY = "14824f88-3cea-4b69-8ebe-e1899a835893"
     AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = "kaliwebs"
